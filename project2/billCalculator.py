2c158bc42e9b379f45d9024c3e4956fab67f3250
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-03-29 21:49:40 -0400

    python day 2 project

---
+++ project2/billCalculator.py
@@ -0,0 +1,11 @@
+def calcTip (total, numPeople, tip):
+    percent = tip/100
+    totalWTip = total * (1+percent)
+    return totalWTip/numPeople
+
+print("Welcome to the Tip Calculator")
+total = float(input("What was the total bill? "))
+numPeople = input("How many people to split the bill ")
+tip = int(input("What percentage tip do you want to give? "))
+amountPerPerson = calcTip(total, numPeople, tip)
+print(f"Each person should pay: ${amountPerPerson}")
