f56195f430a3bdca92300f2290128d7a9d7cfd2b
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-04-02 20:47:13 -0400

    optimized final project

--- day5/passwordGenerator.py
+++ day5/passwordGenerator.py
@@ -8,27 +8,42 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password = ""
+# FIRST TRY
+# password = ""
 
+# for l in range(0, nr_letters):
+#     rand_index = random.randint(0, len(letters)-1)
+#     password += letters[rand_index]
+# for s in range(0, nr_symbols):
+#     rand_index = random.randint(0, len(symbols)-1)
+#     password += symbols[rand_index]
+# for n in range(0, nr_letters):
+#     rand_index = random.randint(0, len(numbers)-1)
+#     password += numbers[rand_index]
+
+# password = list(password)
+
+# for i in range(0,1000):
+#     rand_1 = random.randint(0, len(password)-1)
+#     rand_2 = random.randint(0, len(password)-1)
+
+#     temp= password[rand_1]
+#     password[rand_1] = password[rand_2]
+#     password[rand_2] = temp
+
+# password = ''.join(password)
+# print(f"Your random generated password is: {password}")
+
+# OPTIMIZED
+password = []
 for l in range(0, nr_letters):
-    rand_index = random.randint(0, len(letters)-1)
-    password += letters[rand_index]
+    password.append(random.choice(letters))
+for n in range(0, nr_numbers):
+    password.append(random.choice(numbers))
 for s in range(0, nr_symbols):
-    rand_index = random.randint(0, len(symbols)-1)
-    password += symbols[rand_index]
-for n in range(0, nr_letters):
-    rand_index = random.randint(0, len(numbers)-1)
-    password += numbers[rand_index]
+    password.append(random.choice(symbols))
 
-password = list(password)
-
-for i in range(0,1000):
-    rand_1 = random.randint(0, len(password)-1)
-    rand_2 = random.randint(0, len(password)-1)
-
-    temp= password[rand_1]
-    password[rand_1] = password[rand_2]
-    password[rand_2] = temp
-
+random.shuffle(password)
 password = ''.join(password)
-print(f"Your random generated password is: {password}")
+
+print(f"Your randomized password: {password}")
