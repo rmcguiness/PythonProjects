9688e32df8627d2bb4f4f15b8e057a05a64b25ee
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-04-04 17:57:02 -0400

    prime number checker

--- day8/primeNumberChecker.py
+++ day8/primeNumberChecker.py
@@ -1,8 +1,8 @@
-def prime(number):
+def check_prime(number):
     for i in range(2,number):
         if number%i == 0:
                 return("Not a prime number")
     return(f"{number} is prime")
 
 n = int(input("Input a number ot check if it is prime: \n"))
-print(prime(n))
+print(check_prime(n))
