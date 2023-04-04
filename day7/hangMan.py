00ef42122f17091ee0685343ec185bfe514d342c
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-04-03 20:34:39 -0400

    finished hangman

--- day7/hangMan.py
+++ day7/hangMan.py
@@ -1,25 +1,33 @@
 import random
-word_list = ['ardvark' , 'baboon', 'camel']
+import hangManStages
+import hangManWords
 
+word_list = hangManWords.word_list
+stages = hangManStages.stages
 random_word = random.choice(word_list)
-print(f'The word is {random_word}')
+
 display = ["_" for i in range(len(random_word))]
 alive = True
-lives = 10
+lives = 6
+guessed = []
 while alive:
     index = 0
-    print(display)
+    print(' '.join(display))
+    print(stages[lives])
+    if not len(guessed) == 0:
+        print(f"Previous guesses: \n {' '.join(guessed)}")
     guess = input("Guess a letter: ").lower()
     correct = False
     for letter in random_word:
-        if guess == letter:
+        if guess == letter and letter not in guessed:
             display[index] = letter
             correct = True
         index += 1
-
+    guessed.append(guess)
     if not correct:
         lives -= 1
-
+    if "_" not in display:
+        break
     if lives == 0:
         alive = False
 
