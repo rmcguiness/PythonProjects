8a4e66daef641e6f3dada043be61aa34b00a15a3
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-04-04 18:36:06 -0400

    improved cipher functionality

--- day8/caesarCipher.py
+++ day8/caesarCipher.py
@@ -1,9 +1,4 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def alphabet_shift(shift):
     shifted_alphabet = alphabet[:]
     for i in range(shift):
@@ -26,10 +21,20 @@ def decrypt(code, shift):
         index = shift_alph.index(letter)
         decoded_msg += alphabet[index]
     return(decoded_msg)
+    
+decoding = True
+while decoding:
+    continue_decoding = input('Do you want to continue? y/n \n')
+    if continue_decoding == "n":
+        decoding = False
+        break
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-if direction == 'encode':
-    print(encrypt(text, shift))
-elif direction == 'decode':
-    print(decrypt(text, shift))
-else:
-    print('direction not specified')
+    if direction == 'encode':
+        print(encrypt(text, shift))
+    elif direction == 'decode':
+        print(decrypt(text, shift))
+    else:
+        print('direction not specified')
