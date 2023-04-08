87f9ab393763b6770f411d125564143c4b6fd5e1
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-04-08 12:54:57 -0400

    added start new operation

--- day10_functions_extra/calculator.py
+++ day10_functions_extra/calculator.py
@@ -17,22 +17,30 @@ operations = {
     "*" : multiply,
     "/" : divide,
 }
-calculate = True
-n1 = float(input('Input first number:'))
-while calculate:
-    for op in operations:
-        print(op)
-    operation = input('Input an Operation:')
+def calculator():
+    calculate = True
+    n1 = float(input('Input first number:'))
+    while calculate:
+        for op in operations:
+            print(op)
+        operation = input('Input an Operation:')
+        if operation not in operations:
+            print("Invalid operation entered")
+            calculator()
+            
+        n2 = float(input('Input another number:'))
 
-    n2 = float(input('Input another number:'))
+        function = operations[operation]
+        ans = function(n1, n2)
+        print(f"{n1} {operation} {n2} = {ans}")
 
-    function = operations[operation]
-    ans = function(n1, n2)
-    print(f"{n1} {operation} {n2} = {ans}")
+        cont = input(f'Do you want continue calculating with {ans}?(y/n)To quit press "q". \n')
 
-    cont = input('Do you wish to continue?(y/n) \n')
+        if cont == "n":
+            calculator()
+        elif cont == "y":
+            n1 = ans
+        else:
+            calculate = False
 
-    if cont == "n":
-        calculate = False
-    else:
-        n1 = ans
+calculator()
