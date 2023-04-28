de9c99c75277938fc76a34f89d51a1741ec18ded
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-04-27 21:41:21 -0400

    improved etch-a-sketch

--- day18/etch-a-sketch/main.py
+++ day18/etch-a-sketch/main.py
@@ -16,11 +16,43 @@ def turn_right():
     new_heading = t.heading() - 10
     t.setheading(new_heading)
 
+def circle_forward_left():
+    t.forward(5)
+    new_heading = t.heading() + 5
+    t.setheading(new_heading)
+
+def circle_forward_right():
+    t.forward(5)
+    new_heading = t.heading() - 5
+    t.setheading(new_heading)
+
+def circle_backward_left():
+    t.backward(5)
+    new_heading = t.heading() - 5
+    t.setheading(new_heading)
+
+def circle_backward_right():
+    t.backward(5)
+    new_heading = t.heading() + 5
+    t.setheading(new_heading)
+
+def pen_size_up():
+    og_pen = t.pensize() 
+    t.pensize(og_pen+1)
+
+def pen_size_down():
+    og_pen =   t.pensize() if t.pensize()>0 else 1  
+    t.pensize(og_pen-1)
+
 screen.listen()
 
-screen.onkeypress(move_forward, "w")
-screen.onkeypress(move_backward,"s")
-screen.onkeypress(turn_left, "a")
-screen.onkeypress(turn_right, "d")
+screen.onkey(move_forward, "Up")
+screen.onkey(move_backward,"Down")
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
+screen.onkeypress(t.penup, 'u')
+screen.onkeypress(t.pendown, 'd')
+screen.onkeypress(pen_size_up , '1')
+screen.onkeypress(pen_size_down, '2')
 
 screen.exitonclick()
