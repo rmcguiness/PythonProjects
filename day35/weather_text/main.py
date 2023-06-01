c548143724ff064765bc2d036ba5d95c8083d885
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-05-31 22:07:03 -0400

    updated twilio usage retrieved news articles

--- day35/weather_text/main.py
+++ day35/weather_text/main.py
@@ -1,10 +1,10 @@
 import requests
 import os
 from dotenv import load_dotenv
-# from twilio.rest import Client
-# from twilio.http.http_client import TwilioHttpClient
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 load_dotenv()
-account_sid = "YOUR ACCOUNT SID"
+account_sid = "ACf10c2dec482c6378f9b04b55f12a87bf"
 auth_token = os.environ.get("AUTH_TOKEN")
 omw_api_key = os.environ.get("OWM_API_KEY")
 omw_endpoint = os.environ.get('OWM_ENDPOINT')
@@ -24,14 +24,14 @@ will_rain = [True for weather in hourly_data if weather['weather'][0]['id'] < 70
 
 if will_rain:
     proxy_client = TwilioHttpClient()
-    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+    proxy_client.session.proxies = {'https': os.environ.get('https_proxy')}
 
     client = Client(account_sid, auth_token, http_client=proxy_client)
     message = client.messages \
         .create(
         body="It's going to rain today. Remember to bring an ☔️",
-        from_="YOUR TWILIO VIRTUAL NUMBER",
-        to="YOUR TWILIO VERIFIED REAL NUMBER"
+        from_="18668758481",
+        to="7326758146"
     )
     print(message.status)
 
--- day36/stock-news-normal-start/main.py
+++ day36/stock-news-normal-start/main.py
@@ -2,6 +2,10 @@ import requests
 import os
 import json
 from dotenv import load_dotenv
+from newsapi import NewsApiClient
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
 
 load_dotenv()
 STOCK_NAME = "TSLA"
@@ -10,7 +14,14 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-API_KEY =  os.environ.get('STOCK_API_KEY')
+STOCK_API_KEY =  os.environ.get('STOCK_API_KEY')
+NEWS_API_KEY =  os.environ.get('NEWS_API_KEY')
+
+account_sid = "ACf10c2dec482c6378f9b04b55f12a87bf"
+twilio_auth_token = os.environ.get("AUTH_TOKEN")
+
+newsapi = NewsApiClient(api_key=NEWS_API_KEY)
+
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -19,19 +30,26 @@ API_KEY =  os.environ.get('STOCK_API_KEY')
 req_params = {
     "function":'TIME_SERIES_DAILY_ADJUSTED',
     "symbol":STOCK_NAME,
-    "apikey":API_KEY
+    "apikey":STOCK_API_KEY
 }
 response = requests.get(STOCK_ENDPOINT, params=req_params).json()
 daily_data = [val for (key,val) in response['Time Series (Daily)'].items()]
 
-close_price_1 = daily_data[1]['close']
+close_price_1 = float(daily_data[1]['4. close'])
 #TODO 2. - Get the day before yesterday's closing stock price
-close_price_2 = daily_data[2]['close']
+close_price_2 = float(daily_data[2]['4. close'])
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 diff = abs(close_price_1 - close_price_2)
 #TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-
+percent_diff = close_price_1 - close_price_2/close_price_2
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+
+if(percent_diff > .05):
+    all_articles = newsapi.get_everything(q=f'{STOCK_NAME} OR {COMPANY_NAME}' ,
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page=2)
+    top_three = all_articles['articles'][:3]
 
     ## STEP 2: https://newsapi.org/ 
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
@@ -45,9 +63,16 @@ diff = abs(close_price_1 - close_price_2)
     #to send a separate message with each article's title and description to your phone number. 
 
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
+    
 #TODO 9. - Send each article as a separate message via Twilio. 
-
+    for article in top_three:
+        client = Client(account_sid, twilio_auth_token)
+        message = client.messages \
+            .create(
+            body=f"Headline:{article['title']}. \n Brief:{article['description']} \n {article['url']}",
+            from_="18668758481",
+            to="7326758146"
+        )
 
 
 #Optional TODO: Format the message like this: 
