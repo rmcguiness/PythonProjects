8daf52be348339d80bd17dc41c334c727605ed19
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-06-21 22:08:51 -0400

    added messaging system for price drops on amazon

--- day46/main.py
+++ day46/main.py
@@ -1,4 +1,5 @@
 import requests
+import smtplib
 import lxml
 from bs4 import BeautifulSoup
 
@@ -16,4 +17,20 @@ print(soup.prettify())
 price = soup.find(class_="a-offscreen").get_text()
 price_without_currency = price.split("$")[1]
 price_as_float = float(price_without_currency)
-print(price_as_float)
+
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+BUY_PRICE = 200
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP(YOUR_SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )
